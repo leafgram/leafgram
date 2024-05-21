6f23ba14766c47735ab5980d132d354b39f776c6
president-tuychiyev <president7566@gmail.com>
2024-05-22 01:04:45 +0500

    fix

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controllers;
 
+use Leafgram\TelegramBotApi\Bot;
+
 class TestController extends Controller
 {
     public function index()
     {
+        $bot = new Bot();
+        $bot->sendMessage(677949397, "Test leaftgram");
         response()->json([
             'message' => 'Hi Jony'
         ]);
